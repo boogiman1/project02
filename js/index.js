3c82c6c36378dd78ad01ddb679eb88bf65580e37
boogiman1 <boowon1@naver.com>
2022-08-14 16:46:46 +0900

    contents 3,4,6 추가, footer추가

--- js/index.js
+++ js/index.js
@@ -9,16 +9,15 @@ $(function () {
         firstmenu.find('ul').hide();
         $(this).find('ul').show();
         var submenuHeight=$(this).find('ul').height();
-        header.stop().animate({height:submenuHeight+headerHeight}).css("background"," linear-gradient(#062e6b 100px, rgba(242,242,242,.5) 51px, rgba(242,242,242,.5)")
+        header.stop().animate({height:submenuHeight+headerHeight}).css("background"," linear-gradient(#243c84 100px, rgba(242,242,242,.5) 51px, rgba(242,242,242,.5)")
         h2.css("color","#f2f2f2")
-        // $('overlay').addClass('show')
-    })
+    });
     firstmenu.mouseleave(function() {
         header.stop().animate({height:headerHeight})
         header.css("background","unset")
         h2.css("color","#000")
         h1.css("color","#062e6b")
         $(this).fins('ul').hide();
-        // $('overlay').removeClass('show')
-    })
+    });
+
 })

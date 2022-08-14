9918d7dce3806331962be8f9bb7b7217e6675d3b
boogiman1 <boowon1@naver.com>
2022-08-14 17:31:56 +0900

    top버튼 추가

---
+++ js/topbtn.js
@@ -0,0 +1,16 @@
+const btt=document.querySelector('#top_button')
+let scrollAmount;
+console.log(window)
+window.addEventListener('scroll',function(){
+    scrollAmount=this.scrollY;
+    if(scrollAmount>scrollAmount/7){
+        btt.classList.add('visible');
+    }else{
+        btt.classList.remove('visible');
+    }
+})
+
+btt.addEventListener('click',function(e){
+    e.preventDefault();
+    document.querySelector(btt.getAttribute('href')).scrollIntoView({behavior:'smooth'})
+})

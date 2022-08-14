e9d9bd9e2b4d1d21385afe7fa25559db678e502f
boogiman1 <boowon1@naver.com>
2022-08-15 06:16:47 +0900

    git 오류로 인한 재 업로드

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function () {
+    var firstmenu=$('#header>.inner>nav>ul>li'),
+    header=$('#header'),
+    headerHeight=header.height();
+    h1=$('nav>ul>li:first>a'),
+    h2=$("nav>ul>li>a")
+    fontColor=$('')
+    firstmenu.mouseenter(function(){
+        firstmenu.find('ul').hide();
+        $(this).find('ul').show();
+        var submenuHeight=$(this).find('ul').height();
+        header.stop().animate({height:submenuHeight+headerHeight}).css("background"," linear-gradient(#243c84 100px, rgba(242,242,242,.5) 51px, rgba(242,242,242,.5)")
+        h2.css("color","#f2f2f2")
+    });
+    firstmenu.mouseleave(function() {
+        header.stop().animate({height:headerHeight})
+        header.css("background","unset")
+        h2.css("color","#000")
+        h1.css("color","#062e6b")
+        $(this).fins('ul').hide();
+    });
+
+})
